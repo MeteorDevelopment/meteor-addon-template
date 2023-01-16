a21e8cad541735f705c751b360e937bbf9371bfd
ClosetClient <122790371+ClosetClient@users.noreply.github.com>
2023-01-16 12:15:27 +0100

    Create AutoWTap.java

---
+++ src/main/java/com/example/addon/modules/AutoWTap.java
@@ -0,0 +1,10 @@
+package com.example.addon.modules;
+
+import com.example.addon.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ModuleExample extends Module {
+    public ModuleExample() {
+        super(Addon.CATEGORY, "Closet", "Auto WTap");
+    }
+}
